2e49c98068154503389be2b0a7769be35569dff6
Wilker Dorvelus <wilkerdorvelus@yahoo.com>
2021-03-24 13:31:09 -0400

    fix footer

--- app/DefaultApp/Vues/templates/principale.php
+++ app/DefaultApp/Vues/templates/principale.php
@@ -51,28 +51,9 @@
             </div>
             <ul class="sf-menu header_right">
                 <li class="submenu selected">
-                    <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('accueil')?>" title="HOME">
+                    <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('accueil') ?>" title="HOME">
                         HOME
                     </a>
-                </li>
-                <li class="submenu ">
-                    <a href="#" title="BLOG">
-                        BLOG
-                    </a>
-                    <ul>
-
-                        <li>
-                            <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('blogs')?>" title="Blog 2 columns">
-                                Blog
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('blog')?>" title="SINGLE POST">
-                                Single post
-                            </a>
-                        </li>
-
-                    </ul>
                 </li>
                 <li class="submenu">
                     <a href="#" title="PAGES">
@@ -80,17 +61,17 @@
                     </a>
                     <ul>
                         <li>
-                            <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('about')?>" title="About">
+                            <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('about') ?>" title="About">
                                 About
                             </a>
                         </li>
                         <li>
-                            <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('doctor')?>" title="Our Doctors">
+                            <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('doctor') ?>" title="Our Doctors">
                                 Our Doctors
                             </a>
                         </li>
                         <li>
-                            <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('care')?>" title="Medical Care">
+                            <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('care') ?>" title="Medical Care">
                                 Medical Care
                             </a>
                         </li>
@@ -210,12 +191,12 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('gallery')?>" title="SINGLE POST">
+                    <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('gallery') ?>" title="SINGLE POST">
                         Gallerie
                     </a>
                 </li>
                 <li>
-                    <a href="<?=\app\DefaultApp\DefaultApp::genererUrl('contact')?>" title="SINGLE POST">
+                    <a href="<?= \app\DefaultApp\DefaultApp::genererUrl('contact') ?>" title="SINGLE POST">
                         Contact
                     </a>
                 </li>
@@ -223,18 +204,22 @@
             </ul>
             <div class="mobile_menu">
                 <select>
-                    <option value="<?=\app\DefaultApp\DefaultApp::genererUrl('accueil')?>">HOME</option>
-                    <option value="<?=\app\DefaultApp\DefaultApp::genererUrl('blogs')?>">BLOG</option>
-                    <option value="<?=\app\DefaultApp\DefaultApp::genererUrl('gallery')?>">Gallerie</option>
-                    <option value="<?=\app\DefaultApp\DefaultApp::genererUrl('about')?>">ABOUT</option>
-                    <option value="<?=\app\DefaultApp\DefaultApp::genererUrl('contact')?>">CONTACT</option>
+                    <option value="<?= \app\DefaultApp\DefaultApp::genererUrl('accueil') ?>">HOME</option>
+                    <option value="<?= \app\DefaultApp\DefaultApp::genererUrl('blogs') ?>">BLOG</option>
+                    <option value="<?= \app\DefaultApp\DefaultApp::genererUrl('gallery') ?>">Gallerie</option>
+                    <option value="<?= \app\DefaultApp\DefaultApp::genererUrl('about') ?>">ABOUT</option>
+                    <option value="<?= \app\DefaultApp\DefaultApp::genererUrl('contact') ?>">CONTACT</option>
                 </select>
             </div>
         </div>
     </div>
 
     <?php
-    if(isset($contenue)){echo $contenue;}else{echo "pas de contenue";}
+    if (isset($contenue)) {
+        echo $contenue;
+    } else {
+        echo "pas de contenue";
+    }
     ?>
     <div class="footer_container">
         <div class="footer">
@@ -267,10 +252,13 @@
             <div class="footer_box_container clearfix">
                 <div class="footer_box">
                     <h3 class="box_header">
-                        CHSM
+                        Info
                     </h3>
                     <p class="info">
-                        Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada individual.
+                        CHSM un hopital offrant des soins integres,
+                        6 services 24/24,7/7 soins ambulatoires, urgences,
+                        hospitaliers, chirurgicaux, para-Clinique,
+                        gestion du patient.
                     </p>
                     <ul class="footer_contact_info_container clearfix">
                         <li class="footer_contact_info_row">
@@ -280,51 +268,43 @@
                         </li>
                         <li class="footer_contact_info_row">
                             <div class="footer_contact_info_left">
-                                33 Farlane Street
+                                #43,45 Ave John Brown
                             </div>
                             <div class="footer_contact_info_right">
-                                +123 655 655
+                                +509 28 12 0515
                             </div>
                         </li>
                         <li class="footer_contact_info_row">
                             <div class="footer_contact_info_left">
-                                Keilor East
+                                (Lalue) Port-au-Prince
                             </div>
                             <div class="footer_contact_info_right">
-                                +123 755 755
-                            </div>
-                        </li>
-                        <li class="footer_contact_info_row">
-                            <div class="footer_contact_info_left">
-                                VIC 3033, Australia
-                            </div>
-                            <div class="footer_contact_info_right">
-                                <a href="mailto:CHSM@mail.com" title="Send Email">
-                                    CHSM@mail.com
+                                <a href="mailto:chsmhaiti@gmail.com" title="Send Email">
+                                    chsmhaiti@gmail.com
                                 </a>
                             </div>
+                        </li>
+                        <li class="footer_contact_info_row">
+                            <div class="footer_contact_info_left">
+                                 Haïti
+                            </div>
+
                         </li>
                     </ul>
                     <ul class="social_icons clearfix">
                         <li>
-                            <a class="social_icon facebook" href="http://facebook.com/QuanticaLabs" title=""
+                            <a class="social_icon facebook" href="http://facebook.com/centrehospitaliersaintemarie" title=""
                                target="_blank">
                                 &nbsp;
                             </a>
                         </li>
                         <li>
-                            <a class="social_icon twitter" href="https://twitter.com/QuanticaLabs" title=""
-                               target="_blank">
+                            <a class="social_icon googleplus" href="centrehospitaliersaintemarie" title="">
                                 &nbsp;
                             </a>
                         </li>
                         <li>
-                            <a class="social_icon googleplus" href="#" title="">
-                                &nbsp;
-                            </a>
-                        </li>
-                        <li>
-                            <a class="social_icon mail" href="mailto:CHSM@mail.com" title="">
+                            <a class="social_icon mail" href="mailto:chsmhaiti@gmail.com" title="">
                                 &nbsp;
                             </a>
                         </li>
@@ -334,111 +314,39 @@
                     <div class="clearfix">
                         <div class="header_left">
                             <h3 class="box_header">
-                                Latest Posts
+                              Menu
                             </h3>
                         </div>
-                        <div class="header_right">
-                            <a href="#" id="footer_recent_posts_prev"
-                               class="scrolling_list_control_left icon_small_arrow left_white"></a>
-                            <a href="#" id="footer_recent_posts_next"
-                               class="scrolling_list_control_right icon_small_arrow right_white"></a>
-                        </div>
                     </div>
-                    <div class="scrolling_list_wrapper">
-                        <ul class="scrolling_list footer_recent_posts">
-                            <li class="icon_small_arrow right_white">
-                                <a href="indexb878.html?page=post">
-                                    Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada.
-                                </a>
-                                <abbr title="29 May 2012" class="timeago">29 May 2012</abbr>
-                            </li>
-                            <li class="icon_small_arrow right_white">
-                                <a href="indexb878.html?page=post">
-                                    Lorem ipsum dolor sit amat velum.
-                                </a>
-                                <abbr title="04 Apr 2012" class="timeago">04 Apr 2012</abbr>
-                            </li>
-                            <li class="icon_small_arrow right_white">
-                                <a href="indexb878.html?page=post">
-                                    Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada.
-                                </a>
-                                <abbr title="02 Feb 2012" class="timeago">02 Feb 2012</abbr>
-                            </li>
-                            <li class="icon_small_arrow right_white">
-                                <a href="indexb878.html?page=post">
-                                    Lorem ipsum dolor sit amat velum, consectetur adipiscing elit.
-                                </a>
-                                <abbr title="24 Jan 2011" class="timeago">24 Jan 2011</abbr>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul class="list clearfix">
+                        <li class="icon_small_arrow right_white">
+                            <a href="#"> Home</a>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <a href="#">Medical Care</a>
+                        </li>
+                    </ul>
                 </div>
                 <div class="footer_box last">
                     <div class="clearfix">
                         <div class="header_left">
                             <h3 class="box_header">
-                                Latest Tweets
+                                Liens utiles
                             </h3>
                         </div>
-                        <div class="header_right">
-                            <a href="#" id="latest_tweets_prev"
-                               class="scrolling_list_control_left icon_small_arrow left_white"></a>
-                            <a href="#" id="latest_tweets_next"
-                               class="scrolling_list_control_right icon_small_arrow right_white"></a>
-                        </div>
-                    </div>
-                    <div class="scrolling_list_wrapper">
-                        <ul class="scrolling_list latest_tweets">
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v6.5) of our
-                                    #Timetable #Responsive #Booking #Schedule #WP Plugin. Please check at… <a
-                                        href="https://t.co/yIQXVnMTCJ" target="_blank">t.co/yIQXVnMTCJ</a><abbr
-                                        title="2021-03-15T16:36:30+00:00" class="timeago">Mon Mar 15 16:36:30 +0000
-                                        2021</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v11.4) of our #CSS3
-                                    #WP #Compare #Pricing #Tables #Grids. Please check at #CodeCanyon <a
-                                        href="https://t.co/yNKf2PDY7q" target="_blank">t.co/yNKf2PDY7q</a><abbr
-                                        title="2021-02-02T11:41:53+00:00" class="timeago">Tue Feb 02 11:41:53 +0000
-                                        2021</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v5.2) of our
-                                    Pressroom #News #Magazine #WordPress Theme. Please check at #ThemeForest <a
-                                        href="https://t.co/VSiuakDbGa" target="_blank">t.co/VSiuakDbGa</a><abbr
-                                        title="2021-01-12T14:05:30+00:00" class="timeago">Tue Jan 12 14:05:30 +0000
-                                        2021</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v12.9) of our CHSM
-                                    #Health #Medical #Clinic #WordPress Theme. Please check at… <a
-                                        href="https://t.co/guzeydugYG" target="_blank">t.co/guzeydugYG</a><abbr
-                                        title="2021-01-12T13:27:39+00:00" class="timeago">Tue Jan 12 13:27:39 +0000
-                                        2021</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (8.1) of our Cascade
-                                    #vCard #CV #resume #WordPress Theme. Please check at #ThemeForest <a
-                                        href="https://t.co/R0y8Ippp2R" target="_blank">t.co/R0y8Ippp2R</a><abbr
-                                        title="2020-12-30T15:38:45+00:00" class="timeago">Wed Dec 30 15:38:45 +0000
-                                        2020</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We have released new version (v6.0) of our
-                                    Prestige #Portfolio #CV #vCard #WordPress #Theme Check at #ThemeForest <a
-                                        href="https://t.co/0jRV18Ce6x" target="_blank">t.co/0jRV18Ce6x</a><abbr
-                                        title="2020-12-30T14:15:20+00:00" class="timeago">Wed Dec 30 14:15:20 +0000
-                                        2020</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We have released new version (v7.6) of our
-                                    Nostalgia #Responsive #Portfolio #WordPress #Theme Please check at… <a
-                                        href="https://t.co/p6pEmAEQ9w" target="_blank">t.co/p6pEmAEQ9w</a><abbr
-                                        title="2020-12-29T14:19:30+00:00" class="timeago">Tue Dec 29 14:19:30 +0000
-                                        2020</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v14.1) of our
-                                    GymBase #Gym #Fitness #WordPress Theme. Please check at #ThemeForest <a
-                                        href="https://t.co/8mEwDr0hie" target="_blank">t.co/8mEwDr0hie</a><abbr
-                                        title="2020-12-23T15:20:50+00:00" class="timeago">Wed Dec 23 15:20:50 +0000
-                                        2020</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v12.8) of our CHSM
-                                    #Health #Medical #Clinic #WordPress Theme. Please check at… <a
-                                        href="https://t.co/VWR8W2udDI" target="_blank">t.co/VWR8W2udDI</a><abbr
-                                        title="2020-12-23T15:14:10+00:00" class="timeago">Wed Dec 23 15:14:10 +0000
-                                        2020</abbr></p></li>
-                            <li class="icon_small_arrow right_white"><p>We've released new version (v5.1) of our
-                                    Pressroom #News #Magazine #WordPress Theme. Please check at #ThemeForest <a
-                                        href="https://t.co/VSiuakDbGa" target="_blank">t.co/VSiuakDbGa</a><abbr
-                                        title="2020-12-23T15:05:33+00:00" class="timeago">Wed Dec 23 15:05:33 +0000
-                                        2020</abbr></p></li>
+                        <ul class="list clearfix">
+                            <li class="icon_small_arrow right_white">
+                                <a href="#"> About</a>
+                            </li>
+                            <li class="icon_small_arrow right_white">
+                                <a href="#">Our Doctor</a>
+                            </li>
+                            <li class="icon_small_arrow right_white">
+                                <a href="#"> Gallery</a>
+                            </li>
+                            <li class="icon_small_arrow right_white">
+                                <a href="#"> Contact</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -446,8 +354,8 @@
             <div class="copyright_area clearfix">
                 <div class="copyright_left">
                     © Copyright - <a
-                        href="javascript:void(0)"
-                        title="Centre Hospitalier Sainte Marie" target="_blank">CHSM</a>
+                            href="javascript:void(0)"
+                            title="Centre Hospitalier Sainte Marie" target="_blank">CHSM</a>
                 </div>
                 <div class="copyright_right">
                     <a class="scroll_top icon_small_arrow top_white" href="#top" title="Scroll to top">Top</a>
